ce3e70609e40d10dca4c6c8b2ce63dc988815384
Yann Legendre <yann.legendre@essec.edu>
2019-08-15 12:07:05 +0200

    models

---
+++ app/models/cocktail.rb
@@ -0,0 +1,8 @@
+class Cocktail < ApplicationRecord
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
+
+  validates :name, uniqueness: true, presence: true
+  # before_destroy :check_for_ingredient
+
+end
---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  belongs_to :ingredient
+  belongs_to :cocktail
+
+  validates :description, presence: true
+  validates :cocktail_id, presence: true, uniqueness: { scope: :ingredient_id }
+  validates :ingredient_id, presence: true
+end
